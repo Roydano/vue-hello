6dd59c86afed7bcabc6f5cf1731d3fdfacb68a47
Roydano <roy.dano90@gmail.com>
2021-06-22 09:53:56 +0200

    add bonus metodo img diretto

--- js/script.js
+++ js/script.js
@@ -2,10 +2,24 @@
 //! Bonus: Aggiungere alla pagina un’immagine, presa anch’essa da un data.
 
 
-var app = new Vue({
+// var app = new Vue({
+//     el: '#app',
+//     data: {
+//       messaggio: 'Anche Homer vuole fare Boolean!!',
+//       img:'immagine'
+//     }
+//   });
+
+
+
+
+  // oppure caricando il percorso dell'immagine
+
+  var app = new Vue({
     el: '#app',
     data: {
-      messaggio: 'Anche Homer vuole fare Boolean!!',
-      img:'immagine'
+      messaggio: 'Anche Lisa vuole fare Boolean!!',
+      percorso: 'https://o.aolcdn.com/hss/storage/adam/9a49de67a641651f896d90b69aca8123/lisa%20imac%20java%20server.jpg',
+      alt: 'lisa-developer'
     }
-  })
+  });
